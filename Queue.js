07f30e60c450733d2cf7356e85d0dcaa0731d419
weston-bailey <wdbailey87@gmail.com>
2021-04-05 10:19:00 -0700

    add queues and pies

--- Queue.js
+++ Queue.js
@@ -1,1 +1,62 @@
 // Your Queue Class goes here
+class Queue {
+  constructor() {
+    this.data = []
+  }
+
+  // returns the size of the queue
+  size() {
+    return this.data.length
+  }
+
+  // returns bool if queue is empty
+  isEmpty() {
+    return this.size() === 0
+  }
+
+  // adds item to queue -- returns new size
+  enqueue(item) {
+    this.data.push(item)
+    return this.size()
+  }
+
+  // removes item from queue -- returns new size
+  dequeue(item) {
+    this.data.shift()
+    return this.size()
+  }
+
+  // returns the first item in the queue 
+  peek() {
+    return this.data[0]
+  }
+
+
+  // logs all items in the queue
+  print() {
+    this.data.forEach(element => {
+      console.log(element)
+    })
+  }
+}
+
+/**
+ * testing area
+ */
+
+ console.clear()
+
+ const queuetiePie = new Queue
+ 
+ console.log('.isEmpty() should return true:', queuetiePie.isEmpty()) 
+ console.log('.peek() should return "undefined":', queuetiePie.peek())
+ 
+ console.log('.enqueue() should return 1:', queuetiePie.enqueue('sweetpotato pie'))
+ console.log('.enqueue() should return 2:', queuetiePie.enqueue('banana fudge pie'))
+ console.log('.enqueue() should return 4:', queuetiePie.enqueue('key lime pie'))
+ console.log('.enqueue() should return 3:', queuetiePie.enqueue('sweetpotato pie'))
+ 
+ console.log('.peek() should return "sweetpotato pie":', queuetiePie.peek())
+ console.log('.dequeue() should return 3:', queuetiePie.dequeue())
+ console.log('.peek() should return "banana fudge pie":', queuetiePie.peek())
+ console.log('.print() should log all items in the stack and return undefined:', queuetiePie.print())
--- Stack.js
+++ Stack.js
@@ -48,6 +48,7 @@ console.clear()
 const shortStack = new Stack
 
 console.log('.isEmpty() should return true:', shortStack.isEmpty()) 
+console.log('.peek() should return "undefined":', shortStack.peek())
 
 console.log('.push() should return 1:', shortStack.push('maple syrup'))
 console.log('.push() should return 2:', shortStack.push('butter'))
@@ -58,10 +59,3 @@ console.log('.peek() should return "whipped cream":', shortStack.peek())
 console.log('.pop() should return 3:', shortStack.pop())
 console.log('.peek() should return "fresh berries":', shortStack.peek())
 console.log('.print() should log all items in the stack and return undefined:', shortStack.print())
-
-
-
-
-
-
-// console.log('size() should return 0:', shortStack.size()) 
